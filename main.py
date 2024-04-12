1416326eb5fa33741e90ad368cbadb0d4df49ecf
dkalantar24 <166734042+dkalantar24@users.noreply.github.com>
2024-04-12 11:03:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 print("Welcome to the Notes App!")
 
 def notes():
+  dictionary = {'date': }
+    for item in dictionary:
+      print(item)
   while True:
     date = input("What is today's date?")
     notes = input("Enter your notes:")
-    
+    event[date] = date
   
