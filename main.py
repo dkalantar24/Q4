31248be3f22751162b145fa3cab88fe270d324ec
dkalantar24 <166734042+dkalantar24@users.noreply.github.com>
2024-04-23 11:27:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ dictionary = {'date': }
   for item in dictionary:
     print(item)
 user input = ("Would you like to view a note or create a new one?")
-  if user_input = "view a note"
+if user_input = "view a note":
+  view_note()
+elif user_input = "create a new one":
+  create_note()
 def create_note():
   while True:
     title = input("Enter the title for todays notes:")
     note = input("Enter your notes:")
     dictionary[title] = note
+def view_note():
+  while True:
+    
   
